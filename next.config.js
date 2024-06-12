a8db1443f6ae76348a60c712b5252ef52da6988a
Idnk <efeidukpaye@gmail.com>
2024-06-12 01:06:10 +0100

    shutting off image optimization

--- next.config.js
+++ next.config.js
@@ -3,19 +3,6 @@ module.exports = {
   output: "export",
   distDir: "dist",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    unoptimized: true,
   },
 };
