4246b986c5e078befa2e345b36ca4ef2cfdf317d
Idnk <efeidukpaye@gmail.com>
2025-06-20 12:29:36 +0100

    fix: comment out SidebarRight component in DashboardLayout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -43,7 +43,7 @@ export default function DashboardLayout({
         </header>
         {children}
       </SidebarInset>
-      <SidebarRight />
+      {/* <SidebarRight /> */}
     </SidebarProvider>
   );
 }
