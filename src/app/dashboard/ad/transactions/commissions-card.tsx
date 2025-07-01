14fe6e2a78b2d57fed7c93f8870ef8bcaaa38406
Idnk <efeidukpaye@gmail.com>
2025-07-01 10:30:47 +0100

    feat: update commissions card titles and adjust layout in escrowww transaction page

--- src/app/dashboard/ad/transactions/commissions-card.tsx
+++ src/app/dashboard/ad/transactions/commissions-card.tsx
@@ -3,25 +3,25 @@ import { StatCardV1 } from "@/components/dashboard/stats-card";
 export default function StatCards() {
   const stats = [
     {
-      title: "Total Withdrawals",
+      title: "Current Escrow Balance",
       value: 150000,
     },
     {
-      title: "Pending Withdrawals",
+      title: "Total Escrow Payouts",
       value: 265000,
     },
     {
-      title: "Total Commission",
+      title: "Total Escrow Payings",
       value: 6000,
     },
     {
-      title: "Pending Commission",
+      title: "Total Commission",
       value: 80000,
     },
   ];
 
   return (
-    <div className="grid grid-cols-2 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
       {stats.map((stat, index) => (
         <StatCardV1 key={stat.title} title={stat.title} value={stat.value} />
       ))}
--- src/app/dashboard/ad/transactions/container.tsx
+++ src/app/dashboard/ad/transactions/container.tsx
@@ -2,11 +2,12 @@
 
 import TransactionHistory from "@/app/dashboard/ad/transactions/transactions-history";
 import TransactionOverview from "./transaction-overview";
+import StatCards from "./commissions-card";
 
 export default function WalletContainer() {
   return (
     <>
-      {/* <TransactionOverview/> */}
+      <StatCards />
       <TransactionHistory />
     </>
   );
